374d0251d0551bd7ba6a6df22dcbcaef2daaef19
Julie Chevallier <julye.chevallier@gmail.com>
2025-01-21 10:40:38 +0100

    vire api

---
+++ README.md
@@ -0,0 +1,5 @@
+Objectif :
+
+- Barre de recherche autocomplétion
+- Affichage toutes les cartes types pokédex
+- Affichage détails d'une carte (carte, nom, pv, type, desc?)
--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/generate_playlist', methods=['POST'])
-def generate_playlist():
-    return "Playlist générée !"
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
